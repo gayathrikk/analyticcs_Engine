dc69296a1a487ff6002539339888acb1bdee4b28
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-12-09 10:57:34 +0530

    Update analyticsengine.java

--- src/test/java/Lovya/triple/analyticsengine.java
+++ src/test/java/Lovya/triple/analyticsengine.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
